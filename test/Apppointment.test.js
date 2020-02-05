89ccb4eb3ea8ea7d913ac438bff7bd5a42ee3cc5
Wei-Sheng Cheng <WeiShengCheng@moneydj.com>
2020-02-05 15:41:00 +0800

    Extracting methods in p.34

--- test/Apppointment.test.js
+++ test/Apppointment.test.js
@@ -5,19 +5,20 @@ import { Appointment } from "../src/Appointment";
 describe("Appointment", () => {
   let container;
   let customer;
+  const render = component => ReactDOM.render(component, container);
+
   beforeEach(() => {
     container = document.createElement("div");
   });
   it("renders the customer first name", () => {
     customer = { firstName: "Ashley" };
-    const component = <Appointment customer={customer} />;
-    ReactDOM.render(component, container);
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Ashley");
   });
   it("renders another customer first name", () => {
     customer = { firstName: "Jordan" };
     const component = <Appointment customer={customer} />;
-    ReactDOM.render(component, container);
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Jordan");
   });
 });
