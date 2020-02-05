0ae02657a7ee284e97811731272985cb950b6f48
Wei-Sheng Cheng <WeiShengCheng@moneydj.com>
2020-02-05 14:24:53 +0800

    Add Appointment.js in p.28

--- test/Apppointment.test.js
+++ test/Apppointment.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Appointment } from "../src/Appointment";
 
 describe("Appointment", () => {
   it("renders the customer first name", () => {
