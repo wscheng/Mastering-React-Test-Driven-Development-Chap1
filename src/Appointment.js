66bb7bcf60b43320678681fe5a8d5520bea2a11e
Wei-Sheng Cheng <WeiShengCheng@moneydj.com>
2020-02-05 15:06:28 +0800

    Make test pass updating Appointment.js(2nd edition) in p.32
    The first version is no longer than the second; however, if you're
    counting tokens, it has one less set of curly braces. The most concise solution always wins!

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,2 +1,4 @@
 import React from "react";
-export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
+export const Appointment = ({ customer: { firstName } }) => (
+  <div>{firstName}</div>
+);
