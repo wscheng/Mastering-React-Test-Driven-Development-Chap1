501dc8265bbf46cdfeeb342cc4dfbbb21a14cb3f
Wei-Sheng Cheng <WeiShengCheng@moneydj.com>
2020-02-05 15:25:55 +0800

    Move some initialization in beforeEach() in p.34

--- test/Apppointment.test.js
+++ test/Apppointment.test.js
@@ -5,17 +5,18 @@ import { Appointment } from "../src/Appointment";
 describe("Appointment", () => {
   let container;
   let customer;
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
   it("renders the customer first name", () => {
     customer = { firstName: "Ashley" };
     const component = <Appointment customer={customer} />;
-    container = document.createElement("div");
     ReactDOM.render(component, container);
     expect(container.textContent).toMatch("Ashley");
   });
   it("renders another customer first name", () => {
     customer = { firstName: "Jordan" };
     const component = <Appointment customer={customer} />;
-    container = document.createElement("div");
     ReactDOM.render(component, container);
     expect(container.textContent).toMatch("Jordan");
   });
