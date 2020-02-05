35391b7a0eac92650e6f25b254a216dc3b393ec6
Wei-Sheng Cheng <WeiShengCheng@moneydj.com>
2020-02-05 17:39:29 +0800

    Move date definitions to describe block in p.46

--- test/Apppointment.test.js
+++ test/Apppointment.test.js
@@ -25,6 +25,11 @@ describe("Appointment", () => {
 
 describe("AppointmentsDayView", () => {
   let container;
+  const today = new Date();
+  const appointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) }
+  ];
   beforeEach(() => {
     container = document.createElement("div");
   });
@@ -34,20 +39,10 @@ describe("AppointmentsDayView", () => {
     expect(container.querySelector("div#appointmentsDayView")).not.toBeNull();
   });
   it("renders multiple appointments in an ol element", () => {
-    const today = new Date();
-    const appointments = [
-      { startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) }
-    ];
     render(<AppointmentsDayView appointments={appointments} />);
     expect(container.querySelector("ol").children).toHaveLength(2);
   });
   it("renders each appointments in a li", () => {
-    const today = new Date();
-    const appointments = [
-      { startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) }
-    ];
     render(<AppointmentsDayView appointments={appointments} />);
     expect(container.querySelectorAll("li")).toHaveLength(2);
     expect(container.querySelectorAll("li")[0].textContent).toEqual("12:00");
