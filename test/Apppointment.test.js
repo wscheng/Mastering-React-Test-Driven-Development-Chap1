ce0d3986ae1969d710b9b6bb899466637b2d8149
Wei-Sheng Cheng <WeiShengCheng@moneydj.com>
2020-02-05 14:51:55 +0800

    Make test pass in p.31
    Change the expectation to match on container.textContent.
    Remove the line that calls appendChild on the document body.

--- test/Apppointment.test.js
+++ test/Apppointment.test.js
@@ -7,9 +7,8 @@ describe("Appointment", () => {
     const customer = { firstName: "Ashley" };
     const component = <Appointment customer={customer} />;
     const container = document.createElement("div");
-    document.body.appendChild(container);
     ReactDOM.render(component, container);
-    expect(document.body.textContent).toMatch("Ashley");
+    expect(container.textContent).toMatch("Ashley");
   });
   it.skip("renders another customer first name", () => {
     const customer = { firstName: "Jordan" };
