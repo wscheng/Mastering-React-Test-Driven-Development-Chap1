6ff8c98d8985706bf25e20ce7f79dcb24353afd5
Wei-Sheng Cheng <WeiShengCheng@moneydj.com>
2020-02-05 14:53:41 +0800

    Make it pass updating second case in p.31

--- test/Apppointment.test.js
+++ test/Apppointment.test.js
@@ -10,12 +10,11 @@ describe("Appointment", () => {
     ReactDOM.render(component, container);
     expect(container.textContent).toMatch("Ashley");
   });
-  it.skip("renders another customer first name", () => {
+  it("renders another customer first name", () => {
     const customer = { firstName: "Jordan" };
     const component = <Appointment customer={customer} />;
     const container = document.createElement("div");
-    document.body.appendChild(container);
     ReactDOM.render(component, container);
-    expect(document.body.textContent).toMatch("Jordan");
+    expect(container.textContent).toMatch("Jordan");
   });
 });
